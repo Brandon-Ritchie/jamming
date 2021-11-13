2a29f6dde204bcf17aa017d213a6db4ccc8301a4
Brandon-Ritchie <britchie@brandonritchie.tech>
2021-11-12 19:16:28 -0500

    Passed down and render search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,13 +4,20 @@ import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
 function App() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+    }
+  }
+  
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults />
+          <SearchResults searchResults={this.state.searchResults} />
           <Playlist />
         </div>
       </div>
